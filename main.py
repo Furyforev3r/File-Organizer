1177481d007b41b90bbfb7231232510494b16d14
Messias <88341564+Furyforev3r@users.noreply.github.com>
2023-05-05 19:31:24 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+iniciar = input("Do you want to organize the files? (Y/N)\n- ")
+
+if iniciar.lower() == "y":
+    extensions = {
+        "PNG": "Images",
+        "JPG": "Images",
+        "WEBP": "Images",
+        "MP3": "Musics",
+        "MP4": "Videos",
+    }
+    
+    files = os.listdir()
+
+    for file in files:
+        file_extension = file.split(".")[1]
+        for extension in extensions:
+            if extension.lower() == file_extension.lower():
+                if not os.path.exists(extensions[extension]):
+                    os.makedirs(extensions[extension])
+                else:
+                    pass
+                shutil.move(file, extensions[extension])
+                break
+
+    input("Successfully organized!")
+else:
+    quit()
